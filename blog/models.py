e8720828654255e4e421fa8fe186a088c94b822e
Nilesh Makwana <40433354+nilesh52@users.noreply.github.com>
2024-04-07 22:35:06 +0530

    Building a Blog - Forms, Files & Sessions
    Building a Blog - Forms, Files & Sessions

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Author(models.Model):
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
-    
+
     def __str__(self):
         return self.full_name()
 
@@ -26,10 +26,24 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excert = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    # image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(
-        Author, on_delete=models.SET_NULL,null=True, related_name="posts")
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.title}"
+    
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    
